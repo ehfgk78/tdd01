c0612bcb9e1ac04c0e17cce55dfbbd383f9fa60f
Kim JinSeok <jskim2code@gmail.com>
2018-02-02 13:04:47 +0900

    코드 수정

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -15,3 +15,7 @@ def view_list(request):
     items = Item.objects.all()
     context = {'items': items}
     return render(request, 'list.html', context)
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
